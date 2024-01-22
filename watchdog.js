084d7a33b33ede15ed829e0bdfa400b5b02dee4c
Christof K <krzysztof.kosciuszko@gmail.com>
2024-01-22 13:35:23 +1000

    Revert "geofirestore lib implementation"
    This reverts commit 73cb16645a63c041321f9e56940d53cd3568459d.

--- watchdog.js
+++ watchdog.js
@@ -1,17 +1,15 @@
 const admin = require("firebase-admin");
 const client = require("./qldFuelApiClient");
 const serviceAccount = require("./serviceAccountKey.json");
-const { getFirestore, GeoPoint } = require("firebase-admin/firestore");
+const { getFirestore } = require("firebase-admin/firestore");
 const Firestore = require("@google-cloud/firestore");
 const geofire = require("geofire-common");
-const geofirestore = require("geofirestore")
 
 const app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 const db = getFirestore(app);
-const GeoFirestore = geofirestore.initializeApp(db)
 
 
 
@@ -26,29 +24,23 @@ client.getAllData().then((result) => {
     fuelsRef.doc(`${fuel.FuelId}`).set(fuel);
   })
 
-  // const sitesRef = db.collection("sites");
-  const siteGoeRef = GeoFirestore.collection("sites");
-
+  const sitesRef = db.collection("sites");
   result.sites.forEach(site => {
-    site.coordinates = new GeoPoint(site.Lat, site.Lng);
-    // site.geohash = geofire.geohashForLocation([site.Lat, site.Lng]);
-    // sitesRef.doc(`${site.SiteId}`).set(site);
-    siteGoeRef.doc(`${site.SiteId}`).set(site);
+    site.geohash = geofire.geohashForLocation([site.Lat, site.Lng]);
+    sitesRef.doc(`${site.SiteId}`).set(site);
   })
 
-  const sitePricesGeoRef = GeoFirestore.collection("sites_prices");
-
+  const sitesPricesRef = db.collection("sites_prices");
   result.sites_prices.forEach(sp => {
     const site = result.sites.find(site => site.SiteId == sp.SiteId);
     const parsedDate = new Date(sp.TransactionDateUtc).getTime();
     const key = `${sp.FuelId}_${sp.SiteId}_${parsedDate}`;
 
     if(site) {
-      sp.coordinates = new GeoPoint(site.Lat, site.Lng);
-      // sp.geohash = geofire.geohashForLocation([site.Lat, site.Lng])
+      sp.geohash = geofire.geohashForLocation([site.Lat, site.Lng])
     }
 
-    sitePricesGeoRef.doc(`${key}`).set(sp);
+    sitesPricesRef.doc(`${key}`).set(sp);
   })
 
 });
