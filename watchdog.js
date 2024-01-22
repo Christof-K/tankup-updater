73cb16645a63c041321f9e56940d53cd3568459d
Christof K <krzysztof.kosciuszko@gmail.com>
2024-01-22 11:50:57 +1000

    geofirestore lib implementation

--- watchdog.js
+++ watchdog.js
@@ -1,15 +1,17 @@
 const admin = require("firebase-admin");
 const client = require("./qldFuelApiClient");
 const serviceAccount = require("./serviceAccountKey.json");
-const { getFirestore } = require("firebase-admin/firestore");
+const { getFirestore, GeoPoint } = require("firebase-admin/firestore");
 const Firestore = require("@google-cloud/firestore");
 const geofire = require("geofire-common");
+const geofirestore = require("geofirestore")
 
 const app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 const db = getFirestore(app);
+const GeoFirestore = geofirestore.initializeApp(db)
 
 
 
@@ -24,23 +26,29 @@ client.getAllData().then((result) => {
     fuelsRef.doc(`${fuel.FuelId}`).set(fuel);
   })
 
-  const sitesRef = db.collection("sites");
+  // const sitesRef = db.collection("sites");
+  const siteGoeRef = GeoFirestore.collection("sites");
+
   result.sites.forEach(site => {
-    site.geohash = geofire.geohashForLocation([site.Lat, site.Lng]);
-    sitesRef.doc(`${site.SiteId}`).set(site);
+    site.coordinates = new GeoPoint(site.Lat, site.Lng);
+    // site.geohash = geofire.geohashForLocation([site.Lat, site.Lng]);
+    // sitesRef.doc(`${site.SiteId}`).set(site);
+    siteGoeRef.doc(`${site.SiteId}`).set(site);
   })
 
-  const sitesPricesRef = db.collection("sites_prices");
+  const sitePricesGeoRef = GeoFirestore.collection("sites_prices");
+
   result.sites_prices.forEach(sp => {
     const site = result.sites.find(site => site.SiteId == sp.SiteId);
     const parsedDate = new Date(sp.TransactionDateUtc).getTime();
     const key = `${sp.FuelId}_${sp.SiteId}_${parsedDate}`;
 
     if(site) {
-      sp.geohash = geofire.geohashForLocation([site.Lat, site.Lng])
+      sp.coordinates = new GeoPoint(site.Lat, site.Lng);
+      // sp.geohash = geofire.geohashForLocation([site.Lat, site.Lng])
     }
 
-    sitesPricesRef.doc(`${key}`).set(sp);
+    sitePricesGeoRef.doc(`${key}`).set(sp);
   })
 
 });
